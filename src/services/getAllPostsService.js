4d25c614fbc598d8773955408b89d42e93ab5f7a
GREEN APEX <chandni.yadav@green-apex.com>
2021-05-26 16:26:22 +0530

    first commit

--- src/services/getAllPostsService.js
+++ src/services/getAllPostsService.js
@@ -4,11 +4,5 @@ export const getALLPostsService = async (posts) => {
   return (Axios.get(
     `http://hn.algolia.com/api/v1/search_by_date?tags=story&page=${posts}`  //api URL
   )
-    .then((res) => {
-      return ("API call success---- ", res);                  //Succes 
-    })
-    .catch((e) => {
-      return ("API call not success !Internal Error -----------", e);         //Error
-    })
   )
 };
